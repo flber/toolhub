c1c132a6e2ba0aef2e5e36ddfc81c62997d9fc88
flbr <benrhammond@gmail.com>
2023-05-15 20:56:55 -0700

    text addition works

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use artano::{Annotation, Canvas, Position};
-use image::{self, /*GenericImage, GenericImageView,*/ Luma};
+use image::{self, Luma};
+use magick_rust::{magick_wand_genesis, DrawingWand, MagickWand};
 use qrcode::{EcLevel, QrCode, Version};
-use rusttype::Font;
-use text_to_png::TextRenderer;
+use std::sync::Once;
 
 use std::fs::File;
-use std::io::{BufReader, BufWriter, Read, Result, Write};
+use std::io::{BufReader, Read, Result};
 
-const INPUT: &str = "/home/benh/.ssh/id_rsa.pub";
-const OUTPUT: &str = "./test.png";
-// const TEXT: &str = "./text.png";
+static START: Once = Once::new();
+
+const INPUT: &str = "src/inc/test.pub";
+const OUTPUT: &str = "test.png";
+const TEXT: &str = "Scan this code to reset your password";
 
 fn main() {
 	match encode() {
 		Ok(_) => {
-			// generate_text().unwrap();
 			add_text().unwrap();
 			println!("`{}`", decode());
 		}
@@ -36,49 +36,23 @@ fn encode() -> Result<()> {
 	Ok(())
 }
 
-// fn generate_text() -> Result<()> {
-// 	let renderer = TextRenderer::default();
-// 	let text_img = renderer
-// 		.render_text_to_png_data("hello world", 64, "#000")
-// 		.unwrap();
-// 
-// 	let output_file = File::create(TEXT)?;
-// 	let mut writer = BufWriter::new(output_file);
-// 	writer.write_all(&text_img.data)?;
-// 
-// 	Ok(())
-// }
-
 fn add_text() -> Result<()> {
-	// let images = &[image::open(OUTPUT).unwrap(), image::open(TEXT).unwrap()];
-	// let img_width_out: u32 = images.iter().map(|im| im.width()).max().unwrap_or(0);
-	// let img_height_out: u32 = images.iter().map(|im| im.height()).sum();
-	// let mut imgbuf = image::ImageBuffer::new(img_width_out, img_height_out);
-	// let mut accumulated_height = 0;
-	// for img in images {
-	// 	imgbuf.copy_from(img, accumulated_height, 0);
-	// 	accumulated_height += img.height();
-	// }
-	// imgbuf.save(OUTPUT).unwrap();
-	let f = File::open(OUTPUT)?;
-	let mut reader = BufReader::new(f);
-	let mut buffer = Vec::new();
-	reader.read_to_end(&mut buffer)?;
+	START.call_once(|| {
+		magick_wand_genesis();
+	});
+	let mut wand = MagickWand::new();
+	let mut draw = DrawingWand::new();
+	wand.read_image(OUTPUT).unwrap();
 
-	let mut canvas = Canvas::read_from_buffer(&buffer).unwrap();
+	let width = wand.get_image_width();
+	let height = wand.get_image_height();
+	wand.extend_image(width, height + 64, 0, 0).unwrap();
+	draw.set_font_size(42.0);
+	draw.set_font_weight(32);
+	wand.annotate_image(&draw, 32.0, (height + 28) as f64, 0.0, TEXT)
+		.unwrap();
 
-	let font_data = include_bytes!("inc/DejaVuSans.ttf");
-	let font = Font::try_from_bytes(font_data as &[u8]).unwrap();
-
-	canvas.add_annotation(
-		&Annotation {
-			text: String::from("hello world"),
-			position: Position::Bottom,
-		},
-		&font,
-		1.0,
-	);
-
+	wand.write_image(OUTPUT).unwrap();
 	Ok(())
 }
 
